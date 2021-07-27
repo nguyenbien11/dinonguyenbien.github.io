09d720690b83330e3fe846d628b70a3b3bb14a9d
nguyenbien11 <87993046+nguyenbien11@users.noreply.github.com>
2021-07-27 20:16:42 +0700

    upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const dino = document.getElementById('dino')
+const rock = document.getElementById('rock')
+const score = document.getElementById('score')
+
+function jump() {
+    dino.classList.add('jump-animation');
+    setTimeout(() => {
+        dino.classList.remove('jump-animation');
+    }, 500);
+}
+
+document.addEventListener('keypress', () => {
+    if (!dino.classList.contains('jump-animation')) {
+        jump();
+    }
+})
+
+setInterval(() => {
+    score.innerText++;
+    const dinoTop = parseInt(window.getComputedStyle(dino)
+    .getPropertyValue('top'));
+    const rockLeft = parseInt(window.getComputedStyle(rock)
+    .getPropertyValue('left'));
+    
+    if (rockLeft< 0) {
+        rock.style.display='none';
+    } else {
+        rock.style.display= '';
+    }
+
+    if (rockLeft < 50 && rockLeft >0 && dinoTop > 150) {
+        alert("Thua goy Hòa yêu Hưng óc chó hahahaha!!!" +' ' +score.innerText +' '+ 
+        "\n\nChơi lại nào");
+        location.reload();
+    }
+}, 50);
